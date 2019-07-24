44cb5aa3d2926c500ca9f24c4f4eaf60c6c72904
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-24 16:17:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,9 +9,9 @@ class School
     @roster
   end 
   
-  def add_student(name,grade)
-    @roster[grade] || '=' []
-    @roster[grade] << name 
+  def add_student(student, level)
+    @roster[grade] ||= []
+    @roster[grade] << student 
   end 
   
   def grade(grade)
