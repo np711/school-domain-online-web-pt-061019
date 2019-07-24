0cc14b771ff19500ff86a4433d481b28a7e48eec
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-24 15:51:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -29,5 +29,5 @@ class School
         + it 'is able to retrieve students from a grade' do 
           @school.add_student("Homer Simpson", 9)
           @school.add_student("Avi Flombaum, 10)
-          @school.add_
+          @school.add_student
         
